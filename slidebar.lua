77df32c3a4816dd8e58421d1a7a4e61087987de9
Kidzonio <luciolinsneto@gmail.com>
2025-04-27 20:26:45 -0300

    Code formatting style has been updated

--- slidebar.lua
+++ slidebar.lua
@@ -4,12 +4,12 @@ local screenWidth, screenHeight = guiGetScreenSize ();
 
 Slidebar = {};
 
-function Slidebar:Create (positions, colors, dataSlide)
-    assert (type (positions) == 'table', 'Syntax Error! "Slidebar:Create" function expects "positions" argument to be a "table" value. got ' .. type (positions));
-    assert (type (colors) == 'table', 'Syntax Error! "Slidebar:Create" function expects "colors" argument to be a "table" value. got ' .. type (colors));
-    assert (type (dataSlide) == 'table', 'Syntax Error! "Slidebar:Create" function expects "dataSlide" argument to be a "table" value. got ' .. type (dataSlide));
+function Slidebar:Create(positions, colors, dataSlide)
+    assert(type(positions) == 'table', 'Syntax Error! "Slidebar:Create" function expects "positions" argument to be a "table" value. got ' .. type(positions));
+    assert(type(colors) == 'table', 'Syntax Error! "Slidebar:Create" function expects "colors" argument to be a "table" value. got ' .. type(colors));
+    assert(type(dataSlide) == 'table', 'Syntax Error! "Slidebar:Create" function expects "dataSlide" argument to be a "table" value. got ' .. type(dataSlide));
 
-    local self = setmetatable ({}, { __index = Slidebar });
+    local self = setmetatable({}, { __index = Slidebar });
     
     self.circle = {};
     self.color = {};
@@ -31,20 +31,20 @@ function Slidebar:Create (positions, colors, dataSlide)
 
     self.circleSVG = svgCreate(positions.circle.w, positions.circle.h, [[
         <svg width=']]..(positions.circle.w)..[[' height=']]..(positions.circle.h)..[['>
-        <circle cx=']]..(positions.circle.w/2)..[[' cy=']]..(positions.circle.h/2)..[[' r=']]..(positions.circle.w/2)..[[' fill='white'/>
+        <circle cx=']]..(positions.circle.w/2)..[[' cy=']]..(positions.circle.h / 2)..[[' r=']]..(positions.circle.w / 2)..[[' fill='white'/>
         </svg>
     ]])
 
     return self;
 end
 
-function Slidebar:render ()
+function Slidebar:render()
 
-    local progress = (self.value - self.minValue) / (self.maxValue - self.minValue);
-    local valueX = self.x + (progress * (self.width - self.circle.w));
+    local progress = ( self.value - self.minValue ) / ( self.maxValue - self.minValue );
+    local valueX = self.x + ( progress * ( self.width - self.circle.w ) );
     
-    dxDrawRectangle (self.x, self.y, self.width, self.height, self.color.background);
-    dxDrawRectangle (self.x, self.y, (self.width/1*progress), self.height, self.color.progress);
+    dxDrawRectangle(self.x, self.y, self.width, self.height, self.color.background);
+    dxDrawRectangle(self.x, self.y, (self.width/1*progress), self.height, self.color.progress);
     dxDrawImage(valueX, self.circle.y, self.circle.w, self.circle.h, self.circleSVG, 0, 0, 0, self.color.circle);
 
     if (self.state) then
@@ -53,33 +53,33 @@ function Slidebar:render ()
         local absoluteX = cursor * screenWidth;
         local value = absoluteX - (self.dgOffsetX or 0);
 
-        value = max(self.x, min (value, self.x + self.width - self.circle.w));
+        value = max(self.x, min(value, self.x + self.width - self.circle.w));
 
-        local _progress = (value - self.x) / (self.width - self.circle.w);
+        local _progress = ( value - self.x ) / ( self.width - self.circle.w );
 
-        self.value = self.minValue + _progress * (self.maxValue - self.minValue);
+        self.value = self.minValue + _progress * ( self.maxValue - self.minValue );
     end
 
 end
 
-function Slidebar:setValue (newValue)
+function Slidebar:setValue(newValue)
     if not (newValue) then
-        return false, error ('Syntax Error! "Slidebar:setValue" function expects "newValue" argument to be a "number" value. got ' .. type (newValue));
+        return false, error('Syntax Error! "Slidebar:setValue" function expects "newValue" argument to be a "number" value. got ' .. type(newValue));
     end
 
-    self.value = min (max (newValue, self.minValue), self.maxValue);
+    self.value = min (max(newValue, self.minValue), self.maxValue);
 
     return true;
 end
 
-function Slidebar:getValue ()
-    return (floor (self.value));
+function Slidebar:getValue()
+    return (floor(self.value));
 end
 
-function Slidebar:click (button, state, absoluteX, absoluteY)
+function Slidebar:click(button, state, absoluteX, absoluteY)
     if (button == 'left' and state == 'down') then
 
-        local value = self.circle.x + ((self.value - self.minValue) / (self.maxValue - self.minValue)) * (self.width - self.circle.w);
+        local value = self.circle.x + ( ( self.value - self.minValue ) / ( self.maxValue - self.minValue ) ) * ( self.width - self.circle.w );
 
         if absoluteX >= value and absoluteX <= value + self.circle.w and absoluteY >= self.y and absoluteY <= self.y + self.circle.h then
 
@@ -89,13 +89,13 @@ function Slidebar:click (button, state, absoluteX, absoluteY)
             return true;
         end
 
-        if ((absoluteX > self.x and absoluteX < (self.x + self.width)) and (absoluteY > self.y and absoluteY < (self.y + self.height))) then
+        if ( ( absoluteX > self.x and absoluteX < ( self.x + self.width ) ) and ( absoluteY > self.y and absoluteY < ( self.y + self.height ) ) ) then
 
             _value = max(self.x, min(absoluteX, self.x + self.width - self.circle.w));
 
-            local _progress = (_value - self.x) / (self.width - self.circle.w);
+            local _progress = ( _value - self.x ) / ( self.width - self.circle.w );
 
-            self.value = self.minValue + _progress * (self.maxValue - self.minValue);
+            self.value = self.minValue + _progress * ( self.maxValue - self.minValue );
             self.state = true;
 
             return true;
@@ -113,7 +113,7 @@ end
 
 ---- Example
 
-local slide = Slidebar:Create (
+local slide = Slidebar:Create(
     {
         x = 815,
         y = 543,
@@ -136,11 +136,11 @@ local slide = Slidebar:Create (
     }
 );
 
-addEventHandler ('onClientRender', root, function ()
+addEventHandler('onClientRender', root, function()
     slide:render();
-    dxDrawText (slide:getValue(), 600, 300, 300, 300, tocolor (255, 255, 255, 255), 3);
+    dxDrawText (slide:getValue(), 600, 300, 300, 300, tocolor(255, 255, 255, 255), 3);
 end)
 
-addEventHandler ('onClientClick', root, function (button, state, absoluteX, absoluteY)
-    slide:click (button, state, absoluteX, absoluteY);
+addEventHandler('onClientClick', root, function(button, state, absoluteX, absoluteY)
+    slide:click(button, state, absoluteX, absoluteY);
 end)
